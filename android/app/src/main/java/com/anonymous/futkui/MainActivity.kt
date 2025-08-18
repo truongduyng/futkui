4685c4e64ed684437dec610647865736421e61a6
truongduyng <truongduyng@gmail.com>
2025-08-18 09:36:13 +0700

    refactor(app): update package and bundle identifiers
    Changed the package and bundle identifiers from 'com.anonymous.futkui' to 'com.futkui' across Android and iOS configurations. This update unifies the naming convention and removes 'anonymous' from all relevant namespaces and identifiers, which likely reflects a branding or organizational change. Ensure that any external dependencies or services using these identifiers are updated accordingly.

--- android/app/src/main/java/com/anonymous/futkui/MainActivity.kt
+++ android/app/src/main/java/com/anonymous/futkui/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.anonymous.futkui
+package com.futkui
 import expo.modules.splashscreen.SplashScreenManager
 
 import android.os.Build
--- android/app/src/main/java/com/anonymous/futkui/MainApplication.kt
+++ android/app/src/main/java/com/anonymous/futkui/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.anonymous.futkui
+package com.futkui
 
 import android.app.Application
 import android.content.res.Configuration
